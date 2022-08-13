b6a9ef75ee478b91fe32cb4525ae2413e168dfbd
Vladimir <noise123q@mail.ru>
2022-08-13 08:36:35 +0300

    change way js code

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ document.addEventListener("keydown", function(evt){
 });
 
 //modalBtn
-if (location.pathname == "https://noisekov.github.io/lawyers/"){
+
     const modalBtn = document.querySelector(".header__btn-block-request");
     const modalWindow = document.querySelector(".modal");
     const modalClose = document.querySelector(".modal__btn-close");
@@ -81,7 +81,7 @@ if (location.pathname == "https://noisekov.github.io/lawyers/"){
             modalWindow.classList.remove("show")
         }
     })
-}
+
 
 
 //mask
